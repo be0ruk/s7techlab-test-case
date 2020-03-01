4dd9fe0590f5d56f6f965842a529cc2747a8a7f5
be0ruk <^#4DZdCdo{Jn4a6>
2020-03-02 02:11:00 +0300

    Add viev "all_flights_data"

--- postgres/init.sql
+++ postgres/init.sql
@@ -16,6 +16,7 @@ create unique index airports_iata_uindex
 
 create table flightstats.aircrafts
 (
+    airlinefscode   char(2),
     flightnumber    char(4),
     iata            char(3),
     name            varchar(150),
@@ -36,3 +37,33 @@ create table flightstats.flights
     arrivaltime             timestamp,
     flightequipmentiatacode char(3)
 );
+
+create view flightstats.all_flights_data
+            (upload_at, airline_code, flight_number, departure_time, departure_airport_code, departure_airport_name,
+             departure_city, departure_contry, arrival_time, arrival_airport_code, arrival_airport_name, arrival_city,
+             arrival_country, aircraft_type_code, aircraft_type_name, economy, premium_economy, business, first)
+as
+SELECT fl.upload_at,
+       fl.carrierfscode           AS airline_code,
+       fl.flightnumber            AS flight_number,
+       fl.departuretime           AS departure_time,
+       fl.departureairportfscode  AS departure_airport_code,
+       ap_departure.name          AS departure_airport_name,
+       ap_departure.city          AS departure_city,
+       ap_departure.countryname   AS departure_contry,
+       fl.arrivaltime             AS arrival_time,
+       fl.arrivalairportfscode    AS arrival_airport_code,
+       ap_arrival.name            AS arrival_airport_name,
+       ap_arrival.city            AS arrival_city,
+       ap_arrival.countryname     AS arrival_country,
+       fl.flightequipmentiatacode AS aircraft_type_code,
+       ac.name                    AS aircraft_type_name,
+       ac.economy,
+       ac.premium_economy,
+       ac.business,
+       ac.first
+FROM flightstats.flights fl
+         LEFT JOIN flightstats.airports ap_departure ON fl.departureairportfscode = ap_departure.iata
+         LEFT JOIN flightstats.airports ap_arrival ON fl.arrivalairportfscode = ap_arrival.iata
+         LEFT JOIN flightstats.aircrafts ac
+                   ON fl.carrierfscode = ac.airlinefscode AND fl.flightnumber = ac.flightnumber;
